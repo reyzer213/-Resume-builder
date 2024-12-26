b92e7aca0706701e181e3e66b30ccb6febf4be0c
Oleksey <102412798+reyzer213@users.noreply.github.com>
2024-12-26 11:09:10 +0200

    Last commit

--- myresume/models.py
+++ myresume/models.py
@@ -28,7 +28,7 @@ def save_profile(sender, instance, **kwargs):
 
 
 class Resume(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="resumes")
+    
     title = models.CharField(max_length=100, help_text="Резюме на тему...")
     desire_pos = models.CharField(max_length=100, help_text="На яку посаду ви претендуєте?", null= True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='resumes')
