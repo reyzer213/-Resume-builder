073d2dd3bffc4bf4d3e61f24cc5f8f06b8eb7229
Oleksey <102412798+reyzer213@users.noreply.github.com>
2025-01-14 23:11:30 +0200

    deploy finish5

--- mybuilder/settings.py
+++ mybuilder/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import cloudinary.api
+import environ
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +29,7 @@ SECRET_KEY = 'django-insecure-fp(arh3i_wv@%=p$ey1cs=y-v+gx=6-@m1elnh&c(2nmt@r6e3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -124,6 +127,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env()
 
 CLOUDINARY_STORAGE = "CLOUDINARY_URL=***"
 
